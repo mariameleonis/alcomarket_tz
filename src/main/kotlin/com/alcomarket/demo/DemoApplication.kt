b4ea1b18c2beb43a1bc9c69c572019141f87bacf
Mariya Russakova <russakova.m@gmail.com>
2024-08-07 16:41:58 +0500

    delete unused import

--- src/main/kotlin/com/alcomarket/demo/DemoApplication.kt
+++ src/main/kotlin/com/alcomarket/demo/DemoApplication.kt
@@ -2,7 +2,6 @@ package com.alcomarket.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
